a24fe26df83ab043b7ac22343e0b1c2a9f971d38
Sgeo <sgeoster@gmail.com>
2016-03-15 22:16:18 -0400

    Consistently use `'lock` for the `Lock`'s lifetime.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 use std::marker::PhantomData;
 
-pub struct Lock<'a, T: 'a> {
+pub struct Lock<'lock, T: 'lock> {
     opt_val: Option<T>,
-    phantom_lock: PhantomData<&'a mut ()>
+    phantom_lock: PhantomData<&'lock mut ()>
 }
 
-impl<'a, T: 'a> Lock<'a, T> {
+impl<'lock, T: 'lock> Lock<'lock, T> {
     /// Creates an empty Lock<T> container.
     ///
     /// Needs to be stored in a binding (e.g. `let lock = Lock::new();`) to be useful.
@@ -13,45 +13,45 @@ impl<'a, T: 'a> Lock<'a, T> {
         Lock { opt_val: None, phantom_lock: PhantomData }
     }
     
-    // Needs to be `&'a mut self`, as that makes the Lock's `'a` the same lifetime as the passed-in `&mut self`, causing it to refer to itself.
+    // Needs to be `&'lock mut self`, as that makes the Lock's `'lock` the same lifetime as the passed-in `&mut self`, causing it to refer to itself.
     // Taking `&mut self` instead of `&self` prevents another `Acceptor` from being created, which can help prevent confusion.
-    pub fn lock(&'a mut self) -> Acceptor<'a, T> {
+    pub fn lock(&'lock mut self) -> Acceptor<'lock, T> {
         Acceptor::new(self)
     }
 }
 
-pub struct Acceptor<'a, T: 'a> { 
-    mut_lock: &'a mut Lock<'a, T>
+pub struct Acceptor<'lock, T: 'lock> { 
+    mut_lock: &'lock mut Lock<'lock, T>
 }
 
-impl<'a, T: 'a> Acceptor<'a, T> {
-    fn new(lock: &'a mut Lock<'a, T>) -> Self {
+impl<'lock, T: 'lock> Acceptor<'lock, T> {
+    fn new(lock: &'lock mut Lock<'lock, T>) -> Self {
         Acceptor { mut_lock: lock }
     }
     
-    pub fn fill(self, t: T) -> Handle<'a, T> {
+    pub fn fill(self, t: T) -> Handle<'lock, T> {
         self.mut_lock.opt_val = Some(t);
         Handle { mut_lock: self.mut_lock }
     }
     
-    pub fn fill_from<'b>(self, handle_t: Handle<'b, T>) -> Handle<'a, T> {
+    pub fn fill_from<'other>(self, handle_t: Handle<'other, T>) -> Handle<'lock, T> {
         let new = handle_t.mut_lock.opt_val.take().expect("Attempting to .fill_from() an emptied Handle!");
         self.fill(new)
     }
 }
 
-pub struct Handle<'a, T: 'a> {
-    mut_lock: &'a mut Lock<'a, T>
+pub struct Handle<'lock, T: 'lock> {
+    mut_lock: &'lock mut Lock<'lock, T>
 }
 
-impl<'a, T: 'a> std::ops::Deref for Handle<'a, T> {
+impl<'lock, T: 'lock> std::ops::Deref for Handle<'lock, T> {
     type Target = T;
     fn deref(&self) -> &T {
         self.mut_lock.opt_val.as_ref().expect("Tried to deref() an emptied Handle!")
     }
 }
 
-impl <'a, T: 'a> std::ops::DerefMut for Handle<'a, T> {
+impl<'lock, T: 'lock> std::ops::DerefMut for Handle<'lock, T> {
     fn deref_mut(&mut self) -> &mut T {
         self.mut_lock.opt_val.as_mut().expect("Tried to deref_mut() an emptied Handle!")
     }
